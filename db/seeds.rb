1236bdc858f6d1c5662c2bb38f36a134a36f5719
Kaneko Jun <kanekojun1017@gmail.com>
2024-07-02 01:13:32 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-admin = Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
-  admin.encrypted_password = 'kaneko'  # 適切なパスワードのハッシュ値に置き換えてください
-  admin.reset_password_token = nil
-  admin.reset_password_sent_at = nil
-  admin.remember_created_at = nil
-  admin.created_at = Time.current
-  admin.updated_at = Time.current
-end
+Admin.create(
+  email: 'admin@example.com',
+  encrypted_password: 'kaneko',
+  reset_password_token: nil,
+  reset_password_sent_at: nil,
+  remember_created_at: nil,
+  created_at: Time.now,
+  updated_at: Time.now
+)
